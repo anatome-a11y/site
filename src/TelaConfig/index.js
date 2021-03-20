ade00d16e01dca84147765281d315d86e13ba7d9
habacuqueboy@Protonmail.ch <habacuqueboy@Protonmail.ch>
2021-03-20 15:46:36 -0300

    vc esta em configuracoes

--- src/TelaConfig/index.js
+++ src/TelaConfig/index.js
@@ -3,7 +3,10 @@ import React from 'react'
 
 const TelaConfig = () => {
     return (
-        <div>it works</div>
+        <div style={{marginTop:32,marginBottom:12}}>
+            <span style={{fontWeight:'bold'}}>Você está em: </span> 
+            <span style={{color:'#4facf0'}}>Configurações</span>
+        </div>
     )
 }
 
