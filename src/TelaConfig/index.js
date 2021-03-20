02d90facb8058bf06d44be2ecff7b23316ff189c
habacuqueboy@Protonmail.ch <habacuqueboy@Protonmail.ch>
2021-03-20 16:27:47 -0300

    botoes tela de config

--- src/TelaConfig/index.js
+++ src/TelaConfig/index.js
@@ -1,12 +1,47 @@
-import React from 'react'
+import React , { Fragment } from 'react'
 
+import { Button } from 'antd'
 
-const TelaConfig = () => {
+import Header from './components/Header'
+import Conhecimento from './components/Conhecimento'
+
+const TelaConfig = ({history}) => {
     return (
+        <Fragment>
+
         <div style={{marginTop:32,marginBottom:12}}>
             <span style={{fontWeight:'bold'}}>Você está em: </span> 
             <span style={{color:'#4facf0'}}>Configurações</span>
         </div>
+
+        <div style={{paddingLeft:32,paddingRight:32}}>
+
+            <div style={{display:'flex',width:'100%'}}>
+                <Header style={{flex:1}} title="Entrada" >
+                    <div>teste</div>
+                </Header>
+                <Header style={{flex:1}} title="Apresentação de conhecimento" >
+                    <div>teste</div>
+                </Header>
+            </div>
+
+            <Header style={{flex:1}} title="Interação" >
+                <div>teste</div>
+            </Header>
+
+            <Header style={{flex:1}} title="Máximo de tentativas" >
+                <div>teste</div>
+            </Header>
+
+        </div>
+
+        <div style={{display:'flex',justifyContent:'center',width:'100%',padding:24}}>
+            <Button icon='undo' onClick={ () => history.goBack() } >Voltar</Button>
+            <Button icon='check' style={{marginLeft:8,marginRight:8}} type='secondary'>Salvar configurações</Button>
+            <Button type='primary'>Restaurar padrões</Button>
+        </div>
+
+        </Fragment>
     )
 }
 
