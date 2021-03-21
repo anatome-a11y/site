b80a9aa5872a3d701e62d595ba256f5efc8b8e62
Maycon Carvalho <maycon.arth.carvalho@gmail.com>
2021-03-21 17:58:19 -0300

    ajustando os database local

--- src/TelaCorrecao/avaliacao/db.js
+++ src/TelaCorrecao/avaliacao/db.js
@@ -1,7 +1,7 @@
 import { Avaliacao , Questao , Parte } from './model'
 
-const db = {
-    0 : Avaliacao(
+const db = [
+    Avaliacao(
         0,
         'Maria Eduarda da Silva',
         '05/03/2021',
@@ -11,16 +11,15 @@ const db = {
         'CEFET MG',
         [
             Questao(
-              Parte('Crânio',34,'Maxila','Osso par irregular e suporta a arcada dentária superior'),
-              Parte('Crânio',34,'Maxila','Osso par irregular e pneumático da face lorem impsum etcaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'),
+                Parte('Crânio', 34, 'Maxila', 'Osso par irregular e suporta a arcada dentária superior'),
+                Parte('Crânio', 34, 'Maxila', 'Osso par irregular e pneumático da face lorem impsum etcaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'),
             ),
             Questao(
-              Parte('Teste',33,'Teste2','lorem ipsum dolor'),
-              Parte('Teste',33,'Teste2','lorem ipsum teste teste'),
+                Parte('Teste', 33, 'Teste2', 'lorem ipsum dolor'),
+                Parte('Teste', 33, 'Teste2', 'lorem ipsum teste teste'),
             ),
         ],
     )
-
-}
+]
 
 export default db
