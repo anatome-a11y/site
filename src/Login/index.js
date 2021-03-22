b5d97e7e2c31e92ad7fa925a670569838c260b07
Maycon Carvalho <maycon.arth.carvalho@gmail.com>
2021-03-22 11:38:54 -0300

    create component Login

---
+++ src/Login/index.js
@@ -0,0 +1,76 @@
+import React, {useState} from 'react'
+import {Button, Input} from "antd";
+
+import iconLock from './lock-icon.svg';
+
+const TelaLogin = ({history}) => {
+
+    const [userName, setUserName] = useState(null);
+    const [password, setPassword] = useState(null);
+
+    return (
+        <div style={styles.center}>
+            <div style={styles.container}>
+                <div style={styles.center}>
+                    <div style={styles.circle}>
+                        <img style={styles.image} src={iconLock} alt='asd'/>
+                    </div>
+                </div>
+                <div style={styles.input}>
+                    <span>Usuário</span>
+                    <Input value={userName} onChange={e => setUserName(e.target.value)} />
+                </div>
+                <div style={styles.input}>
+                    <span>Senha</span>
+                    <Input value={password} onChange={e => setPassword(e.target.value)} />
+                </div>
+                <div style={styles.center}>
+                    <Button
+                        style={styles.button}
+                        type='primary'
+                        onClick={() => history.push('/')}
+                    >Entrar</Button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+const styles = {
+    container: {
+        marginTop: '50px',
+        backgroundColor: '#E8E8E8',
+    },
+    circle: {
+        width: 70,
+        height: 70,
+        borderRadius: '50%',
+        backgroundColor: '#e518a7',
+        marginTop: '15px',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    center: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    button: {
+        paddingLeft: '85px',
+        paddingRight: '85px',
+        marginTop: '15px',
+        marginBottom: '20px'
+    },
+    input: {
+        marginLeft: '30px',
+        marginRight: '30px',
+        marginBottom: '25px'
+    },
+    image: {
+        width: '40px',
+        height: '40px'
+    }
+}
+
+export default TelaLogin
