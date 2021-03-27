ca400a2eda10e4c814c038de5d2ba561343c0815
Simon-Isenmann <isenmann1997@gmail.com>
2021-03-27 18:06:22 -0300

    questoes

--- src/TelaCorrecao/avaliacao/db.js
+++ src/TelaCorrecao/avaliacao/db.js
@@ -11,12 +11,16 @@ const db = [
         'CEFET MG',
         [
             Questao(
-                Parte('Crânio', 34, 'Maxila', 'Osso par irregular e suporta a arcada dentária superior'),
-                Parte('Crânio', 34, 'Maxila', 'Osso par irregular e pneumático da face lorem impsum etcaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'),
+                Parte('Crânio', 9, 'Maxila', 'Osso par irregular e suporta a arcada dentária superior'),
+                Parte('Crânio', 9, 'Maxila', 'Osso par, irregular e pneumático. Articula-se com o nasal, palatino, etmoide, frontal, zigomático, lacrimal, vômer e concha nasal inferior. Suportam os dentes superiores e forma uma parte do palato, da cavidade nasal e da órbita ocular.'),
             ),
             Questao(
-                Parte('Teste', 33, 'Teste2', 'lorem ipsum dolor'),
-                Parte('Teste', 33, 'Teste2', 'lorem ipsum teste teste'),
+                Parte('Crânio', 2, 'Parietal', 'Osso par, laminar, localizado nas laterais superiores do crânio.'),
+                Parte('Crânio', 2, 'Parietal', 'Osso par, laminar, localizado nas laterais superiores do crânio.'),
+            ),
+            Questao(
+                Parte('Crânio', 8, 'Zigomático', 'Osso par, irregular. Localiza-se na região da bochecha e é o mais proeminente da face.'),
+                Parte('Crânio', 8, 'Zigomático', 'Osso par, irregular. Localiza-se na região da bochecha e é o mais proeminente da face.'),
             ),
         ],
     )
